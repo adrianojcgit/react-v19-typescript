c17740621e9cea10a0465c541d431139517a16ba
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-27 18:16:50 -0300

    Demonstrar como usar o Hooks useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+'use client'
 import Menu from "@/components/Menu";
 import Footer from "@/components/Footer";
 import User from "@/components/User";
+import { useState } from "react";
 
 const Home = () => {
-  const userName = "Adriano";
+  const [nameUser, setNameUser] = useState("Adriano");
+
   return (
     <div>
       <Menu /><br/>
-      <User name={userName}>
+      <User name={nameUser}>
         <p>Este é um conteúdo extra fornecido como children.</p>
       </User>
       <h2>Bem-vindo!!</h2><br />
+      <button onClick={() => setNameUser("Nicolas")}>Alterar</button>
       <Footer />
     </div>
   );
